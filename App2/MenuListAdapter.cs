c52128b59d30f8cb8198f32f3e9485d4bf17a9ba
Benoit Gidon <gidon@intechinfo.fr>
2016-06-17 10:51:49 +0200

    Imported Solution

---
+++ App2/MenuListAdapter.cs
@@ -0,0 +1,86 @@
+using System;
+using Android.App;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Views.Animations;
+using System.Linq;
+using Android.Graphics;
+
+namespace App2
+{
+    public class MenuListAdapterClass : BaseAdapter<string>
+    {
+        Activity _context;
+        string[] _mnuText;
+        int[] _mnuUrl;
+        //action event to pass selected menu item to main activity
+        internal event Action<string> actionMenuSelected;
+        public MenuListAdapterClass(Activity context, string[] strMnu, int[] intImage)
+        {
+            _context = context;
+            _mnuText = strMnu;
+            _mnuUrl = intImage;
+        }
+        public override string this[int position]
+        {
+            get { return this._mnuText[position]; }
+        }
+
+        public override int Count
+        {
+            get { return this._mnuText.Count(); }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            MenuListViewHolderClass objMenuListViewHolderClass;
+            View view;
+            view = convertView;
+            if (view == null)
+            {
+                view = _context.LayoutInflater.Inflate(Resource.Layout.MenuCustomLayout, parent, false);
+                objMenuListViewHolderClass = new MenuListViewHolderClass();
+
+                objMenuListViewHolderClass.txtMnuText = view.FindViewById<TextView>(Resource.Id.txtMnuText);
+                objMenuListViewHolderClass.ivMenuImg = view.FindViewById<ImageView>(Resource.Id.ivMenuImg);
+
+                objMenuListViewHolderClass.initialize(view);
+                view.Tag = objMenuListViewHolderClass;
+            }
+            else
+            {
+                objMenuListViewHolderClass = (MenuListViewHolderClass)view.Tag;
+            }
+            objMenuListViewHolderClass.viewClicked = () =>
+            {
+                if (actionMenuSelected != null)
+                {
+                    actionMenuSelected(_mnuText[position]);
+                }
+            };
+            objMenuListViewHolderClass.txtMnuText.Text = _mnuText[position];
+            objMenuListViewHolderClass.ivMenuImg.SetImageResource(_mnuUrl[position]);
+            return view;
+        }
+    }
+    //Viewholder class
+    internal class MenuListViewHolderClass : Java.Lang.Object
+    {
+        internal Action viewClicked { get; set; }
+        internal TextView txtMnuText;
+        internal ImageView ivMenuImg;
+        public void initialize(View view)
+        {
+            view.Click += delegate
+            {
+                viewClicked();
+            };
+        }
+
+    }
+}
